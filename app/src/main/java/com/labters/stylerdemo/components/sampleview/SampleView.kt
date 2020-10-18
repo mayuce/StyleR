4c90d25f18a4a6beee64ac6a60ee066c89b54db0
Ali YÜCE <yucee76@gmail.com>
2020-10-18 23:33:53 +0300

    [UPD][ali.yuce][SampleView new styles added.]

--- app/src/main/java/com/labters/stylerdemo/components/sampleview/SampleView.kt
+++ app/src/main/java/com/labters/stylerdemo/components/sampleview/SampleView.kt
@@ -11,6 +11,7 @@ package com.labters.stylerdemo.components.sampleview
 
 import android.content.Context
 import android.util.AttributeSet
+import com.labters.styler.core.StyleR
 import com.labters.stylerdemo.BaseView
 import com.labters.stylerdemo.R
 import com.labters.stylerdemo.databinding.ViewSampleBinding
@@ -18,9 +19,32 @@ import com.labters.stylerdemo.databinding.ViewSampleBinding
 class SampleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : BaseView<ViewSampleBinding>(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val STYLE_NORMAL = "SampleView"
+        private const val STYLE_REVERSE = "SampleView.Reverse"
+        private const val STYLE_HUGE = "SampleView.HugeHeader"
+    }
+
+    private var styleName = STYLE_NORMAL
+        set(value) {
+            field = value
+            applyStyleDif()
+        }
+
+    private fun applyStyleDif() {
+        StyleR.applyStyle(binding.root, styleName)
+    }
+
     override fun initView() {
         binding.run {
-
+            root.setOnClickListener {
+                styleName = when (styleName) {
+                    STYLE_NORMAL -> STYLE_REVERSE
+                    STYLE_REVERSE -> STYLE_HUGE
+                    else -> STYLE_NORMAL
+                }
+            }
         }
     }
 
